b3be4d712a28f6b675ddb57befe6786ca3fa9fe8
LeoKnop <eloknop@gmail.com>
2024-06-27 13:52:28 +0200

    header files nun am richtigen Ort

--- lib/init.h
+++ lib/init.h
@@ -1,1 +1,11 @@
+#ifndef INIT_H
+#define INIT_H
+
 // INIT -> Alle Initfunktionen werden hier deklariert
+void initMC(void);
+void initPWM(void);
+void initR2R(void);
+void initSPI(void);
+void initUART(void);
+
+#endif
